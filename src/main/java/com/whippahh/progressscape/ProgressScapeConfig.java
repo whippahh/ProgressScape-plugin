015d4df86aa43ca2eb6d16b54cf7590eab3bc958
whippahh <whippahh@gmail.com>
2026-03-19 23:18:55 +1100

    Update ProgressScapeConfig.java

--- src/main/java/com/whippahh/progressscape/ProgressScapeConfig.java
+++ src/main/java/com/whippahh/progressscape/ProgressScapeConfig.java
@@ -2,31 +2,8 @@ package com.whippahh.progressscape;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("progressscape")
 public interface ProgressScapeConfig extends Config
 {
-	@ConfigItem(
-			keyName = "supabaseUrl",
-			name = "Supabase URL",
-			description = "Your Supabase project URL",
-			position = 1
-	)
-	default String supabaseUrl()
-	{
-		return "https://hbfnvijfjboxhamjmlhm.supabase.co";
-	}
-
-	@ConfigItem(
-			keyName = "supabaseKey",
-			name = "Supabase Anon Key",
-			description = "Your Supabase anon public key",
-			position = 2,
-			secret = true
-	)
-	default String supabaseKey()
-	{
-		return "";
-	}
 }
